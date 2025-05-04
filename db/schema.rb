19ef37a73384884a195d80613cff36f3b29f0e18
yfukumoto15 <yfukumoto@netprotections.co.jp>
2025-05-04 12:13:02 +0000

    Add user microposts

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2025_05_04_044814) do
+ActiveRecord::Schema[7.0].define(version: 2025_05_04_101422) do
+  create_table "microposts", force: :cascade do |t|
+    t.text "content"
+    t.integer "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_microposts_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
@@ -27,4 +35,5 @@ ActiveRecord::Schema[7.0].define(version: 2025_05_04_044814) do
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
+  add_foreign_key "microposts", "users"
 end
